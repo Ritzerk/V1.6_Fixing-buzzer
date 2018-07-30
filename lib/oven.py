5704d7d2a97b687917897582c0afec1dc43d5e53
Ritzerk <41624795+Ritzerk@users.noreply.github.com>
2018-07-30 13:22:00 +0100

    Update oven.py

--- lib/oven.py
+++ lib/oven.py
@@ -121,7 +121,7 @@ class Oven (threading.Thread):
 		#This is where we may ask user to open vent to cool, new function to pop window.
                 #self.set_cool(pid <= -1)	#Returns false or true, not required.
 		if (self.target < (last_temp + 30)):
-		    self.coolMsg = "NOW":	
+		    self.coolMsg = "NOW"	
 		    self.set_buzz(True)
 		    time.sleep(0.5)
 		    self.set_buzz(False)
